01f9b53d221eef02795f55839184e1b20eff061e
ErnestKurbonov <ernestkurbonov@gmail.com>
2024-03-30 21:06:50 +0400

    inicial commit

---
+++ src/FirstVideoCourse/HomeWork.java
@@ -0,0 +1,17 @@
+package FirstVideoCourse;
+
+public class HomeWork {
+    public static void main(String[]args){
+        System.out.println("              Rubai                 ");
+        System.out.println("Mnogo let razmishlyal ya nad jizmiy zemnoy.");
+        System.out.println("Neponytnogo net dlya menya pod lunoy");
+        System.out.println("Mne izvestno shto mne nichego ne izvestno!");
+        System.out.println("Vot vsya pravda otritaya mnoy.");
+        System.out.println("                        O.Xayam");
+        // TODO: 30.03.2024  
+    }
+    
+
+}
+
+
---
+++ src/FirstVideoCourse/MethodsPrintPrintln.java
@@ -0,0 +1,37 @@
+package FirstVideoCourse;
+
+public class MethodsPrintPrintln {
+    public static void main(String[] args) {
+        System.out.print("my name is ");
+        System.out.println("Izzatullo");
+
+        System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c");
+
+        System.out.println("Aaaaaa       Bbbbbbbb       Ccccc");
+
+        System.out.println("absdef");
+        System.out.println();//метод println без какого либо параметра будет отображать пустую строку
+        System.out.println("ghijkl");
+    }
+}
+// TODO: 30.03.2024
+/*print выводит на дисплей необходимую информацию которая заключена в двойных скобакхах и в двойных  ковычках все что
+* находиться в скобках наз параметром метода*/
+
+/*println этот метод тоже выводит на дисплей необходимую информацию и переводит курсор на следующую строку*/
+
+/*Джава это обьектно оринтирований язык програмированя*/
+
+/*Джава являеться языком высокого уровня которая использует компилятор для того чтобы переобразовать наш код в байт код
+с расширением точка класс */
+
+/*Байт код с расширением точка класс обрабатыветься затем JVM и с помощью интерпретора и получается из этого байт кода
+машинный код который затем запускается*/
+
+/*Метод main явл стартовой точкой любой джава програмы если приложение не содержит метод main мы его не сможем запустить
+public static void main(String[]args{our code};  */
